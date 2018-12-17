204928a9f67fcfd238caa603fad73f00523c2ddf
Lakshmikanthan <lakshmikanthan.ananth@imaginea.com>
2018-12-17 22:06:13 +0530

    Modified ListOrganizationRepo class

--- src/test/java/com/github/api/ListOrganizationRepoTest.java
+++ src/test/java/com/github/api/ListOrganizationRepoTest.java
@@ -1,5 +1,31 @@
 package com.github.api;
 
+import io.restassured.RestAssured;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import io.restassured.response.Response;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
 public class ListOrganizationRepoTest {
+    public  String org="SprinklrOrg";
+    @BeforeClass
+    public void setBaseUri () {
+
+        RestAssured.baseURI = "https://api.github.com";
+        PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
+        authScheme.setUserName("PramatiImaginea");
+        authScheme.setPassword("Sprinklr123");
+        RestAssured.authentication = authScheme;
+    }
+@Test
+        public void getList_OrgRepo(){
+    Response res= given()
+            .header("content-type", "application/json").param("org",org)
+            .when()
+            .get("/orgs/:org/repos").then().extract().response();
+    System.out.println(res.asString());
+}
 
 }
