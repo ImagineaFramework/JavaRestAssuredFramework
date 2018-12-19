d822a6ae42daae0b6b882aa33795bfad791c85f1
jayaprakashr <jayaprakash.rajendiran@imaginea.com>
2018-12-19 17:48:13 +0530

    Change some respository

--- src/main/java/com/github/api/ListPublicRespository.java
+++ src/main/java/com/github/api/ListPublicRespository.java
@@ -1,8 +1,10 @@
 package com.github.api;
 
+import com.google.gson.JsonObject;
 import io.restassured.RestAssured;
 import io.restassured.authentication.PreemptiveBasicAuthScheme;
 import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 import org.testng.Assert;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -25,7 +27,7 @@ public class ListPublicRespository {
         RestAssured.authentication = authScheme;
     }
 
-    @Test
+    @Test(enabled = true)
     public void getPublicRespository() {
         Response response = given()
                 .header("content-type", "application/json")
@@ -36,36 +38,44 @@ public class ListPublicRespository {
         System.out.println("Status Code is "+statusCode);
         Assert.assertEquals(statusCode,200);
         System.out.println("Response code "+response.asString());
+
     }
 
    @Test(enabled = false)
     public void postPublicRespository() {
 
-       String name= "Hello-World";
+
+
+       String jsonBody = "{" +
+               "\"name\": \"Hello-World\"," +
+               "\"description\": \"This is your first repository\","+
+               "\"homepage\": \"https://github.com\"" +
+               "\"private\": false,"+
+               "\"has_issues\": true,"+
+               "\"has_projects\": true,"+
+               "\"has_wiki\": true  ,"+
+               "}";
+
+
+       /*String name= "Hello-World";
        String description = "This is your first repository";
         String homepage= "https://github.com";
         boolean privatee= false;
         boolean  has_issues= true;
         boolean  has_projects= true;
-        boolean  has_wiki=true;
+        boolean  has_wiki=true;*/
 
         Response response = given()
                 .header("content-type", "application/json")
-                .body(name)
-                .body(description)
-                .body(homepage)
-                .body(privatee)
-                .body(has_issues)
-                .body(has_projects)
-                .body(has_wiki)
-                .expect()
-                .body("status",notNullValue())
+                .body(jsonBody)
                 .when()
                 .post("/user/repos");
+
         int statusCode=response.getStatusCode();
         System.out.println("Status Code is "+statusCode);
         Assert.assertEquals(statusCode,201);
         System.out.println("Response code "+response.asString());
     }
+
 
 }
