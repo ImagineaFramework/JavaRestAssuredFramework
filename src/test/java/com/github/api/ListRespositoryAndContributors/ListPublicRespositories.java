8abe027c8fb4add49d7a6ad331c568e29d36264f
jayaprakashr <jayaprakash.rajendiran@imaginea.com>
2018-12-20 17:15:44 +0530

    Modified changes

--- src/test/java/com/github/api/ListRespositoryAndContributors/ListPublicRespositories.java
+++ src/test/java/com/github/api/ListRespositoryAndContributors/ListPublicRespositories.java
@@ -3,6 +3,9 @@ package com.github.api.ListRespositoryAndContributors;
 import io.restassured.RestAssured;
 import io.restassured.authentication.PreemptiveBasicAuthScheme;
 import io.restassured.response.Response;
+import io.restassured.response.ResponseBody;
+import io.restassured.specification.RequestSpecification;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -23,47 +26,18 @@ public class ListPublicRespositories {
         RestAssured.authentication = authScheme;
     }
 
-    @Test
+    @Test()
     public void getPublicRespositories() {
+
         Response response = given()
                 .header("content-type", "application/json")
                 .when()
                 .get("/repositories").then().
                         extract().response();
-        int statusCode=response.getStatusCode();
-        System.out.println("Status Code is "+statusCode);
-        Assert.assertEquals(statusCode,200);
-        System.out.println("Response code "+response.asString());
-    }
-
-    @Test(enabled = false)
-    public void postPublicRespository() {
-
-        String name= "Hello-World";
-        String description = "This is your first repository";
-        String homepage= "https://github.com";
-        boolean privatee= false;
-        boolean  has_issues= true;
-        boolean  has_projects= true;
-        boolean  has_wiki=true;
-
-        Response response = given()
-                .header("content-type", "application/json")
-                .body(name)
-                .body(description)
-                .body(homepage)
-                .body(privatee)
-                .body(has_issues)
-                .body(has_projects)
-                .body(has_wiki)
-                .expect()
-                .body("status",notNullValue())
-                .when()
-                .post("/user/repos");
-        int statusCode=response.getStatusCode();
-        System.out.println("Status Code is "+statusCode);
-        Assert.assertEquals(statusCode,201);
-        System.out.println("Response code "+response.asString());
+        int statusCode = response.getStatusCode();
+        System.out.println("Status Code is " + statusCode);
+        Assert.assertEquals(statusCode, 200);
+        System.out.println("Response code " + response.asString());
     }
 
 }
