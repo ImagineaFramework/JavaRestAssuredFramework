142acd3fb69f3c5c9228557f9a63712c02f633a5
jayaprakashr <jayaprakash.rajendiran@imaginea.com>
2019-01-02 14:02:16 +0530

    Added changes

---
+++ src/test/java/com/github/api/ListRespositoryAndContributors/GetSamePatternRepo.java
@@ -0,0 +1,45 @@
+package com.github.api.ListRespositoryAndContributors;
+
+import io.restassured.RestAssured;
+import io.restassured.authentication.PreemptiveBasicAuthScheme;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class GetSamePatternRepo {
+
+   @BeforeMethod
+    public void setBaseUri() {
+
+        RestAssured.baseURI = "https://api.github.com";
+        PreemptiveBasicAuthScheme authScheme = new PreemptiveBasicAuthScheme();
+        authScheme.setUserName("PramatiImaginea");
+        authScheme.setPassword("Sprinklr123");
+        RestAssured.authentication = authScheme;
+    }
+
+    @Test
+
+    public void getSamePatternRespositories() {
+
+        Response response = RestAssured.given()
+                .queryParam("q","pramati")
+                .header("content-type", "application/json")
+                .when()
+                .get("/search/repositories")
+                .then().
+                        extract().response();
+        int statusCode=response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+        System.out.println("Status Code  "+statusCode);
+        System.out.println("Response code " + response.asString());
+
+    }
+
+
+
+
+}
---
+++ src/test/java/com/github/api/ListRespositoryAndContributors/ParseXMLInputOutput.java
@@ -0,0 +1,4 @@
+package com.github.api.ListRespositoryAndContributors;
+
+public class ParseXMLInputOutput {
+}
